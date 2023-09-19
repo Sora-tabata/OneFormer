148a1deaf73f9ee6db0c7976f6f363cc845261de
Sora-tabata <sora48691525@gmail.com>
2023-09-19 21:07:19 +0900

    update build_container.sh

--- build_container.sh
+++ build_container.sh
@@ -14,7 +14,7 @@ docker run -i -t --gpus all -d \
 --name="oneformer_v2" sorajang/oneformer_v2 bash
 
 # Git pull orbslam and compile
-docker exec -it oneformer bash -i -c "apt-get update && \
+docker exec -it oneformer_v2 bash -i -c "apt-get update && \
     apt-get upgrade && \
     apt-get install libgl1-mesa-dev && \
     apt-get install libglib2.0-0 && \
