169f0ca4a29b1b2443fbf85e128ebfb151c0286a
Sora-tabata <sora48691525@gmail.com>
2023-09-20 02:15:49 +0000

    update build_container.sh

--- demo/demo.sh
+++ demo/demo.sh
@@ -1,6 +1,6 @@
 export task=semantic
-python demo.py --config-file ../configs/mapillary_vistas/dinat/oneformer_dinat_large_bs16_300k.yaml \
+python demo.py --config-file ../configs/cityscapes/convnext/mapillary_pretrain_oneformer_convnext_xlarge_bs16_90k.yaml \
   --input /mnt/source/datasets/img_230607/* \
   --output /mnt/source/datasets/shibuya_oneformer/ \
   --task $task \
-  --opts MODEL.IS_TRAIN False MODEL.IS_DEMO True MODEL.WEIGHTS /mnt/source/OneFormer/250_16_dinat_l_oneformer_mapillary_300k.pth
+  --opts MODEL.IS_TRAIN False MODEL.IS_DEMO True MODEL.WEIGHTS /mnt/source/OneFormer/model_final.pth
