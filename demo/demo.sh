0b31b1b469614cb03d5734ececfc19aa33e00b52
Sora-tabata <sora48691525@gmail.com>
2023-09-21 12:15:41 +0000

    modified cityscapes_evaluation.py

--- demo/demo.sh
+++ demo/demo.sh
@@ -1,6 +1,6 @@
-export task=semantic
+export task=panoptic
 python demo.py --config-file ../configs/cityscapes/convnext/mapillary_pretrain_oneformer_convnext_xlarge_bs16_90k.yaml \
-  --input /mnt/source/datasets/img_230607/* \
+  --input /mnt/source/datasets/stuttgart_00/* \
   --output /mnt/source/datasets/shibuya_oneformer/ \
   --task $task \
-  --opts MODEL.IS_TRAIN False MODEL.IS_DEMO True MODEL.WEIGHTS /mnt/source/OneFormer/model_final.pth
+  --opts MODEL.IS_TRAIN False MODEL.IS_DEMO True MODEL.WEIGHTS /mnt/source/OneFormer/model/model_ceymo20230921.pth
