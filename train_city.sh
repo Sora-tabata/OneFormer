df0cd7ae3a9898c589c2c52ec3f953a4afe9b72a
Sora-tabata <sora48691525@gmail.com>
2023-10-03 09:48:54 +0000

    changed train_city.sh

--- train_city.sh
+++ train_city.sh
@@ -2,7 +2,7 @@ export DETECTRON2_DATASETS="/mnt/source/datasets"
 #export PYTORCH_CUDA_ALLOC_CONF="max_split_size_mb:24"
 #python3 oneformer/data/datasets/register_mapillary_vistas_panoptic.py
 
-python train_net.py --dist-url 'tcp://127.0.0.1:50163' \
+python train_net.py --dist-url 'tcp://127.0.0.1:50165' \
     --num-gpus 1 \
     --config-file configs/cityscapes/convnext/mapillary_pretrain_oneformer_convnext_xlarge_bs16_90k.yaml \
-    OUTPUT_DIR outputs/ceymo WANDB.NAME ceymo
+    OUTPUT_DIR outputs/ceymo2 WANDB.NAME ceymo
